7628c7f0af9a654cd71839abb217464091486dfd
Dileep Rajput <dileep_rajput@yahoo.com>
2018-10-10 14:46:54 -0400

    Add APIAttribution component to give credit to random quotes source

---
+++ src/components/APIAttribution.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function APIAttribution(props) {
+  return (
+    <span>
+      <p>Quotes provided by <a href="https://talaikis.com/">Tadas Talaikis</a> and his awesome <a href="https://talaikis.com/random_quotes_api/">Random Quotes API</a></p>
+    </span>
+  );
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "../css/App.css";
-import QuoteBox from './QuoteBox.js'
+import QuoteBox from './QuoteBox.js';
+import APIAttribution from './APIAttribution.js';
 
 class App extends Component {
   render(){
     return(
       <div className="App">
         <QuoteBox />
+        <APIAttribution />
       </div>
     );
   }
