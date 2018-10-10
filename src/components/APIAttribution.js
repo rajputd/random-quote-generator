b3abf3fd8356e4b58c408b7ce40119fe1f5bef6c
Dileep Rajput <dileep_rajput@yahoo.com>
2018-10-10 17:01:42 -0400

    Style APIAttribution component

--- src/components/APIAttribution.js
+++ src/components/APIAttribution.js
@@ -1,8 +1,9 @@
 import React from "react";
+import '../css/APIAttribution.css';
 
 export default function APIAttribution(props) {
   return (
-    <span>
+    <span id="APIAttribution">
       <p>Quotes provided by <a href="https://talaikis.com/">Tadas Talaikis</a> and his awesome <a href="https://talaikis.com/random_quotes_api/">Random Quotes API</a></p>
     </span>
   );
